fc307a1e561aad4d1efe44e5d9848984039dec7b
crebro <creationduwal@gmail.com>
2022-02-06 16:31:02 +0545

    fix: fixed a typo on the quote section

--- components/pages/Home/Quote/index.js
+++ components/pages/Home/Quote/index.js
@@ -5,7 +5,7 @@ function DeveloperQuote() {
     <div className="text-4xl sm:text-5xl text-center font-poppins font-black"> What It&apos;s all about</div>
     <p className='w-[90%] lg:w-1/2 md:w-2/3 font-nunito italic text-center mt-4 text-xl'> &quot;As a beginner streamer, I have always wanted to have all the
         professional looking streaming elements on my stream, but most of them
-        happen to be paid, so BackSlash was created just so that won&apos; happen
+        happen to be paid, so BackSlash was created just so that won&apos;t happen
         to anyone again&quot; - Kreation Duwal, Developer
     </p>
   </div>
